d7ca03e5c50dedf6d9be1075d58dedbf2c93462c
VanshPatelD55 <138886011+VanshPatelD55@users.noreply.github.com>
2023-08-24 18:21:36 +0530

    Add files via upload

---
+++ Walker.py
@@ -0,0 +1,34 @@
+import pygwalker as pyg
+import pandas as pd
+import streamlit.components.v1 as components
+import streamlit as st
+ 
+# Adjust the width of the Streamlit page
+st.set_page_config(
+    page_title="Use Pygwalker In Streamlit",
+    layout="wide"
+)
+ 
+# Add Title
+st.title("Use Pygwalker In Streamlit")
+ 
+# Import your data
+st.session_state.df = None
+if st.session_state.df is None:
+    uploaded_file = st.file_uploader(
+        "Choose a CSV file. This should be in long format (one datapoint per row).",
+        type="csv",
+    )
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.session_state.df = df
+ 
+
+
+with st.form("demo"):
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        with st.spinner():
+            pyg_html = pyg.walk(st.session_state.df, return_html=True)
+            components.html(pyg_html, height=1000, scrolling=True)
+
